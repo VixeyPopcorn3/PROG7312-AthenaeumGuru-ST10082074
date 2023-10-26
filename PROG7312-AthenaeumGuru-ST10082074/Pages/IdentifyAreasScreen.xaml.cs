8794f6f90c33977285edca3435a04469e87ecbf9
VixeyPopcorn3 <134959380+VixeyPopcorn3@users.noreply.github.com>
2023-10-26 20:34:35 +0200

    IdentifyingAreas Alles works

--- PROG7312-AthenaeumGuru-ST10082074/Pages/IdentifyAreasScreen.xaml.cs
+++ PROG7312-AthenaeumGuru-ST10082074/Pages/IdentifyAreasScreen.xaml.cs
@@ -35,18 +35,20 @@ namespace PROG7312_AthenaeumGuru_ST10082074.Pages
         private bool keyside;
         private int Score, CumulativeScore, Counter; 
         private bool checkButtonClicked = false;
+        private List<ListBoxItem> selectedItems = new List<ListBoxItem>();
 
 
         public IdentifyAreasScreen()
         {
             InitializeComponent();
+            LeftBox.Background = null; RightBox.Background = null;
             keyside = random.Next(2) == 0;
             identifyingAreas = new IdentifyingAreas();
             PopulateLeftBoxWithItems(LeftBox);
             PopulateRightBoxWithRandomItems(RightBox);
         }
-        
 
+        /*
         private void LeftBox_SelectionChanged(object sender, SelectionChangedEventArgs e)//changing the colour of the selected item
         {
             ListBox leftBox = (ListBox)sender;
@@ -105,8 +107,75 @@ namespace PROG7312_AthenaeumGuru_ST10082074.Pages
                     }
                 }
             }
+        }*/
+        private void LeftBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListBox leftBox = (ListBox)sender;
+
+            if (leftBox.SelectedItem != null)
+            {
+                lastSelectedItem = (ListBoxItem)leftBox.ItemContainerGenerator.ContainerFromItem(leftBox.SelectedItem);
+
+                if (!lastSelectedRightItems.ContainsKey(lastSelectedItem))
+                {
+                    lastSelectedRightItems[lastSelectedItem] = null; // Initialize with null
+                }
+                ListBoxItem selectedListBoxItem = (ListBoxItem)leftBox.ItemContainerGenerator.ContainerFromItem(leftBox.SelectedItem);
+
+                // Check if we have a previously stored background color for the item
+                if (itemBackgroundColors.ContainsKey(selectedListBoxItem))
+                {
+                    // Restore the stored background color
+                    selectedListBoxItem.Background = itemBackgroundColors[selectedListBoxItem];
+                }
+                else
+                {
+                    // Generate a random color for the item
+                    Color randomColor = Color.FromRgb((byte)random.Next(256), (byte)random.Next(256), (byte)random.Next(256));
+                    SolidColorBrush brush = new SolidColorBrush(randomColor);
+
+                    // Store the background color for the item
+                    itemBackgroundColors[selectedListBoxItem] = brush;
+
+                    // Set the item's background color
+                    selectedListBoxItem.Background = brush;
+                }
+
+                // Add the selected item to the list
+                if (!selectedItems.Contains(selectedListBoxItem))
+                {
+                    selectedItems.Add(selectedListBoxItem);
+                }
+            }
         }
-        
+
+        private void RightBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListBox rightBox = (ListBox)sender;
+
+            if (rightBox.SelectedItem != null && lastSelectedItem != null)
+            {
+                ListBoxItem selectedListBoxItem = (ListBoxItem)rightBox.ItemContainerGenerator.ContainerFromItem(rightBox.SelectedItem);
+
+                if (lastSelectedRightItems.ContainsKey(lastSelectedItem))
+                {
+                    // Reset the previous right item's color
+                    if (lastSelectedRightItems[lastSelectedItem] != null)
+                    {
+                        lastSelectedRightItems[lastSelectedItem].Background = Brushes.Transparent;
+                    }
+
+                    // Set the background color of the current right item
+                    lastSelectedRightItems[lastSelectedItem] = selectedListBoxItem;
+                    if (itemBackgroundColors.ContainsKey(lastSelectedItem))
+                    {
+                        selectedListBoxItem.Background = itemBackgroundColors[lastSelectedItem];
+                    }
+                }
+            }
+        }
+
+
         private void PopulateLeftBoxWithItems(ListBox listBox)
         {
             listBox.Items.Clear(); // Clear the list box first
@@ -192,36 +261,37 @@ namespace PROG7312_AthenaeumGuru_ST10082074.Pages
             identifyingAreas = new IdentifyingAreas();
             PopulateLeftBoxWithItems(LeftBox);
             PopulateRightBoxWithRandomItems(RightBox);
-
-
-
-
-            //Check = false;
+            checkButtonClicked = false;
         }
         private void CheckBtn_Click(object sender, RoutedEventArgs e)
         {
             if (!checkButtonClicked)
             {
-                int selectedCount = GetSelectedCount();
-                if (selectedCount < 4)
+                int selectedCount = selectedItems.Count;
+                if (selectedCount == 0)
+                {
+                    MessageBox.Show("You haven't selected any answers. Please select at least one answer before checking.");
+                    checkButtonClicked = false;
+                }
+                else if (selectedCount < 4)
                 {
                     MessageBox.Show("You haven't completed all the questions, to get a better score next time try answering them all.");
+                    checkButtonClicked = false;
                 }
                 else
                 {
                     CalculateScore();
                     CumulativeScore = CumulativeScore + Score;
                     CumulativeScorelbl.Content = "Cumulative Score: " + CumulativeScore;
+                    checkButtonClicked = true;
                 }
 
-                checkButtonClicked = true;
+                
             }
             else
             {
-                MessageBox.Show("Please Reset, You cant play a single game over and over, Rather Learn with a new game.");
-
+                MessageBox.Show("Please Reset, You can't play a single game over and over. Learn with a new game.");
             }
-
         }
         private void HelpBtn_Click(object sender, EventArgs e)
                 {
@@ -293,30 +363,6 @@ namespace PROG7312_AthenaeumGuru_ST10082074.Pages
             }
 
             return false;
-        }
-        private int GetSelectedCount()
-        {
-            int selectedCount = 0;
-
-            foreach (var leftItem in LeftBox.Items)
-            {
-                foreach (var rightItem in RightBox.Items)
-                {
-                    if (leftItem is string && rightItem is string)
-                    {
-                        ListBoxItem leftListBoxItem = (ListBoxItem)LeftBox.ItemContainerGenerator.ContainerFromItem(leftItem);
-                        ListBoxItem rightListBoxItem = (ListBoxItem)RightBox.ItemContainerGenerator.ContainerFromItem(rightItem);
-
-                        // Check if both items have the same background color (user's idea of a match).
-                        if (leftListBoxItem.Background == rightListBoxItem.Background)
-                        {
-                            selectedCount++;
-                        }
-                    }
-                }
-            }
-
-            return selectedCount;
         }
     }
 }
